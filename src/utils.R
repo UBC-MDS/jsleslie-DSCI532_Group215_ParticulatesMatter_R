a09069acdc36e44c010ecb6d6b611c5264e1d6f4
tphaterp <tphaterp@gmail.com>
2019-12-11 18:48:04 -0800

    started implementing rolling averages

--- src/utils.R
+++ src/utils.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(plotly)
+library(tibbletime)
 
 ## TO DO:
 # - add comments/docstrings
@@ -7,16 +8,18 @@ library(plotly)
 # - move the read_csv calls to app.R
 
 
-# pollution_df <- read_csv("../data/processed_data.csv",
-#                col_types = cols_only(index = col_date(),
-#                                      STATION_NAME = col_factor(),
-#                                      PARAMETER = col_factor(),
-#                                      RAW_VALUE = col_double()))
+pollution_df <- read_csv("data/processed_data.csv",
+               col_types = cols_only(index = col_date(),
+                                     STATION_NAME = col_factor(),
+                                     PARAMETER = col_factor(),
+                                     RAW_VALUE = col_double()))
 
-# df_avg <- read_csv("../data/processed_baseline_data.csv",
-#                   col_types = cols_only(index = col_date(),
-#                                      PARAMETER = col_factor(),
-#                                      RAW_VALUE = col_double()))
+df_avg <- read_csv("data/processed_baseline_data.csv",
+                  col_types = cols_only(index = col_date(),
+                                     PARAMETER = col_factor(),
+                                     RAW_VALUE = col_double()))
+
+# temp_data$RAW_VALUE <- stats::filter(temp_data$RAW_VALUE, rep(1/5,5), method = "convolution", sides = 2)
 
 ###### Plot 1: Location Linechart
 
@@ -24,6 +27,7 @@ location_linechart <- function(data, avg_data, pm = "PM25", init_locations= list
   
   temp_data <- data %>% filter(PARAMETER == pm, STATION_NAME %in% init_locations)
   temp_avg <- avg_data %>% filter(PARAMETER == pm)
+  temp_data$RAW_VALUE <- stats::filter(temp_data$RAW_VALUE, rep(1/5,5), method = "convolution", sides = 2)
   
   ggplot(temp_data, aes(x = index, y = RAW_VALUE, color = STATION_NAME)) + 
   geom_line(data = temp_avg, aes(x = index, y = RAW_VALUE), color = "grey") +
@@ -42,8 +46,15 @@ location_linechart <- function(data, avg_data, pm = "PM25", init_locations= list
 linechart <- function(data, avg_data, init_locations= list(), width = NULL, height = NULL, daterange= list(2000,2017)){
   
   temp_data <- data %>% filter(STATION_NAME %in% init_locations)
-  temp_PM10 <- temp_data %>% filter(PARAMETER == "PM10")
-  temp_PM25 <- temp_data %>% filter(PARAMETER == "PM25")
+  #nested_df <- nest(temp_data,-PARAMETER) %>% mutate(data = map(data, function(x) rollmean(x$RAW_VALUE, 5, na.pad = TRUE)))
+  # temp_PM10 <- temp_data %>% 
+  #   filter(PARAMETER == "PM10") %>% 
+  #   mutate(RAW_VALUE = stats::filter(RAW_VALUE, rep(1/5,5), method = "convolution", sides = 2))
+  # temp_PM25 <- temp_data %>% 
+  #   filter(PARAMETER == "PM25") %>%
+  #   mutate(RAW_VALUE = stats::filter(RAW_VALUE, rep(1/5,5), method = "convolution", sides = 2))
+    
+
   
   ggplot(temp_data, aes(x = index, y = RAW_VALUE, color = PARAMETER)) +
   geom_line(data = avg_data, aes(x = index, y = RAW_VALUE, group = PARAMETER), color = "grey") +
